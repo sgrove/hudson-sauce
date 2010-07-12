911f5def31e67b3f360266af8891aded69bd36fe
Sean Grove <sean@chuwe.com>
2010-07-11 22:32:30 -0700

    Small edit for hudson compatibility

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,22 +1,13 @@
 from selenium import selenium
 import unittest
 
-raw = open("saucelabs_account", "r").read().partition("\n")
+raw = open("tsts/saucelabs_account", "r").read().partition("\n")
 
 saucelabs_username = raw[0]
 saucelabs_password = raw[2][0:-1]
 
 class sauce(unittest.TestCase):
     def setUp(self):
-        print """{\
-                "username": "%s",\
-                "access-key": "%s",\
-                "os": "Windows 2003",\
-                "browser": "firefox",\
-                "browser-version": "3.",\
-                "job-name": "This is an example test"\
-                }""" % (saucelabs_username, saucelabs_password)
-                 
         self.browser = selenium(
                 'saucelabs.com',
                 4444,
